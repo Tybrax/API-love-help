6dc71110e51116e46a95ec1ec6f62c8de8f813d0
Bastien <bastien.ratat@gmail.com>
2020-07-19 16:57:23 +0200

    add validations to models for requests and respectives tests & fixtures

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,2 +1,16 @@
 class Request < ApplicationRecord
+
+    #Presence
+    validates :title, :request_type, :description, :location, presence: true
+
+    #Type
+    validates :title, :request_type, :description, :location, format: { with: /\A[a-zA-Z]+\z/,
+    message: "only allows letters" }
+    validates :id, numericality: { only_integer: true }
+    validates :fulfilled, inclusion: [true, false]
+
+    #Length
+    validates_length_of :title, minimum: 10, maximum: 30
+    validates_length_of :description, minimum: 30, maximum: 300
+
 end
--- test/controllers/requests_controller_test.rb
+++ test/controllers/requests_controller_test.rb
@@ -5,34 +5,152 @@ class RequestsControllerTest < ActionDispatch::IntegrationTest
     @request = requests(:one)
   end
 
+
   test "should get index" do
-    get requests_url, as: :json
+    get '/api/v1/requests', as: :json
     assert_response :success
   end
 
-  test "should create request" do
-    assert_difference('Request.count') do
-      post requests_url, params: { request: {  } }, as: :json
+  # Data types
+  test "should not create if title is not string" do
+    assert_no_difference('Request.count') do
+      post '/api/v1/requests', params: { request: {
+        id: 3451,
+        title: 123901249,
+        request_type: "One-time task",
+        description: "This is the description of the request. Keep in mind that the description is expected to exceed 30 characters in order to be added to the backend DB. Also, it's supposed to have a length inferior to 300 characters. Make sure it's the case to have a valid input.",
+        location: "43.290165506, 5.36916519",
+        fullfilled: false
+        }
+      }, as: :json
     end
-
-    assert_response 201
   end
 
-  test "should show request" do
-    get request_url(@request), as: :json
-    assert_response :success
-  end
-
-  test "should update request" do
-    patch request_url(@request), params: { request: {  } }, as: :json
-    assert_response 200
-  end
-
-  test "should destroy request" do
-    assert_difference('Request.count', -1) do
-      delete request_url(@request), as: :json
+  test "should not create if type is not string" do
+    assert_no_difference('Request.count') do
+      post '/api/v1/requests', params: { request: {
+        id: 3451,
+        title: "please help me",
+        request_type: true,
+        description: "This is the description of the request. Keep in mind that the description is expected to exceed 30 characters in order to be added to the backend DB. Also, it's supposed to have a length inferior to 300 characters. Make sure it's the case to have a valid input.",
+        location: "43.290165506, 5.36916519",
+        fullfilled: false
+        }
+      }, as: :json
     end
-
-    assert_response 204
   end
+
+  test "should not create if description is not string" do
+    assert_no_difference('Request.count') do
+      post '/api/v1/requests', params: { request: {
+        id: 3451,
+        title: "please help me",
+        request_type: "One-time task",
+        description: true,
+        location: "43.290165506, 5.36916519",
+        fullfilled: false
+        }
+      }, as: :json
+    end
+  end
+
+  test "should not create if location is not string" do
+    assert_no_difference('Request.count') do
+      post '/api/v1/requests', params: { request: {
+        id: 3451,
+        title: "please help me",
+        request_type: "One-time task",
+        description: "This is the description of the request. Keep in mind that the description is expected to exceed 30 characters in order to be added to the backend DB. Also, it's supposed to have a length inferior to 300 characters. Make sure it's the case to have a valid input.",
+        location: 1,
+        fullfilled: false
+        }
+      }, as: :json
+    end
+  end
+
+  test "should not create if id is not an integer" do
+    assert_no_difference('Request.count') do
+      post '/api/v1/requests', params: { request: {
+        id: "random number",
+        title: 123901249,
+        request_type: "One-time task",
+        description: "This is the description of the request. Keep in mind that the description is expected to exceed 30 characters in order to be added to the backend DB. Also, it's supposed to have a length inferior to 300 characters. Make sure it's the case to have a valid input.",
+        location: "43.290165506, 5.36916519",
+        fullfilled: false
+        }
+      }, as: :json
+    end
+  end
+
+  test "should not create if fulfilled is not a boolean" do
+    assert_no_difference('Request.count') do
+      post '/api/v1/requests', params: { request: {
+        id: 3451,
+        title: 123901249,
+        request_type: "One-time task",
+        description: "This is the description of the request. Keep in mind that the description is expected to exceed 30 characters in order to be added to the backend DB. Also, it's supposed to have a length inferior to 300 characters. Make sure it's the case to have a valid input.",
+        location: "43.290165506, 5.36916519",
+        fullfilled: "random string, not a boolean"
+        }
+      }, as: :json
+    end
+  end
+
+  # Input length
+  test "should not create if title is below 10 characters" do
+    assert_no_difference('Request.count') do
+      post '/api/v1/requests', params: { request: {
+        id: 3451,
+        title: "help",
+        request_type: "One-time task",
+        description: "This is the description of the request. Keep in mind that the description is expected to exceed 30 characters in order to be added to the backend DB. Also, it's supposed to have a length inferior to 300 characters. Make sure it's the case to have a valid input.",
+        location: "43.290165506, 5.36916519",
+        fullfilled: false
+        }
+      }, as: :json
+    end
+  end
+
+  test "should not create if title is more than 50 characters" do
+    assert_no_difference('Request.count') do
+      post '/api/v1/requests', params: { request: {
+        id: 3451,
+        title: "this title is very long and aims at testing if the user is writing a too detailed title. If he has more info to write, there's a description textarea!",
+        request_type: "One-time task",
+        description: "This is the description of the request. Keep in mind that the description is expected to exceed 30 characters in order to be added to the backend DB. Also, it's supposed to have a length inferior to 300 characters. Make sure it's the case to have a valid input.",
+        location: "43.290165506, 5.36916519",
+        fullfilled: false
+        }
+      }, as: :json
+    end
+  end
+
+  test "should not create if description is less than 30 characters" do
+    assert_no_difference('Request.count') do
+      post '/api/v1/requests', params: { request: {
+        id: 3451,
+        title: "please help me",
+        request_type: "One-time task",
+        description: (0...21).map { ('a'..'z').to_a[rand(26)] }.join,
+        location: "43.290165506, 5.36916519",
+        fullfilled: false
+        }
+      }, as: :json
+    end
+  end
+
+  test "should not create if description is more than 300 characters" do
+    assert_no_difference('Request.count') do
+      post '/api/v1/requests', params: { request: {
+        id: 3451,
+        title: "please help me",
+        request_type: "One-time task",
+        description: (0...350).map { ('a'..'z').to_a[rand(26)] }.join,
+        location: "43.290165506, 5.36916519",
+        fullfilled: false
+        }
+      }, as: :json
+    end
+  end
+
 end
