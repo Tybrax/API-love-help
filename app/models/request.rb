536d3b118cf9a40707de2c75a3455cee635a5e1d
Bastien <bastien.ratat@gmail.com>
2020-07-25 23:13:52 +0200

    temporary removed validations for the sake of designing the db associations

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,15 +1,3 @@
 class Request < ApplicationRecord
 
-    #Presence
-    validates :title, :request_type, :description, :location, presence: true
-
-    #Type
-    #validates :title, :description, format: { with: /\A[a-zA-Z]+\z/,
-    #message: "only allows letters" }
-    validates :fulfilled, inclusion: [true, false]
-
-    #Length
-    validates_length_of :title, minimum: 10, maximum: 30
-    validates_length_of :description, minimum: 30, maximum: 300
-
 end
